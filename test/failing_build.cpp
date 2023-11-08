c9daad7ae8d66c0704ab42dbd52aec840e7173ab
x-mass <36629999+x-mass@users.noreply.github.com>
2023-11-08 10:29:54 +0000

    Testing consistent testing

---
+++ test/failing_build.cpp
@@ -0,0 +1,11 @@
+#define BOOST_TEST_MODULE crypto3_marshalling_failing_build_test
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(failing_suite)
+
+BOOST_AUTO_TEST_CASE(failing_case) {
+    BOOST_CHECK(1==0);
+}
+asdfs
+BOOST_AUTO_TEST_SUITE_END()
---
+++ test/fixed_by_consistent_testing.cpp
@@ -0,0 +1,13 @@
+#define BOOST_TEST_MODULE crypto3_marsalling_fixed_by_consistent_testing_test
+
+#include <boost/test/unit_test.hpp>
+#include <nil/marshalling-clone/include/nil/marshalling/units.hpp>
+
+BOOST_AUTO_TEST_SUITE(failing_suite)
+
+BOOST_AUTO_TEST_CASE(failing_case) {
+    // get_courics() exists not in master
+    BOOST_CHECK(nil::marshalling::units::get_courics()==0);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
